1386fdf419438825854ea8ab93b1203ef606bb33
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-16 14:37:26 +0300

    whoam i?

---
+++ src/components/main.jsx
@@ -0,0 +1,25 @@
+export default function Main(){
+    return(
+        <main>
+            <div className="whMe">
+                <h1>Who am I ?</h1>
+                <ul>
+                    <li>Name: Mazen Fathy</li>
+                    <li>Age: 17</li>
+                    <li>Career: Frontend developer</li>
+                    <li>Skills: HTML, CSS, JS</li>
+                    <li>Currently learning: React libirary</li>
+                </ul>
+                <h1>What is this website responsibel for?</h1>
+                <p>
+                        I think any website that talks about a specific technology is made to help beginners explore it.
+                    However, the person who made this website is also a beginner in React.
+                    So, to be clear, I made this website to practice React and make sure that I am able to build static webpages using it.
+                    Finally, if there are any experts reading this, please don't roast me because I am using React to build a static webpage 🙏
+                </p>
+            </div>
+            <div></div>
+            <div></div>
+        </main>
+    )
+}
