2383f86cee0d4450595ca42030a50ef8b45b3681
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-16 15:13:10 +0300

    changing css folder location

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "./styles/main.css"
+import "./styles/header.css"
+import "./styles/mainSection.css"
 createRoot(document.getElementById('root')).render(
     <App />
 )
