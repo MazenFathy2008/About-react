7174210db07f0790f18941960287928e48f8c246
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-16 15:40:01 +0300

    fixing images

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,13 @@
 export default function Header(){
     return(
     <header>
-        <img src="React-icon.svg" alt="react logo" width="40px" />
+        <img src="/React-icon.svg" alt="react logo" width="40px" />
         <ul>
             <li>Who am I ?</li>
             <li>What Is react ?</li>
             <li>Why to use react ?</li>
         </ul>
-        <img src="React-icon.svg" alt="react logo" width="40px" />
+        <img src="/React-icon.svg" alt="react logo" width="40px" />
     </header>
 )
 }
